8e0675bf31302994ffed69cecf431f83b0f30815
WorkRock <wbsrl3532@naver,com>
2022-07-08 14:44:02 +0900

    STG_Prac_So

---
+++ STG_Prac_So/Assets/BG_Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_Move : MonoBehaviour
+{
+    public float speed;
+    public Vector3 limit;
+    public Vector3 spawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < limit.y)
+        {
+            transform.position = new Vector3(spawn.x, spawn.y, spawn.z);
+        }
+        
+        transform.Translate(0, (-1) * speed * Time.deltaTime, 0);
+    }
+}
---
+++ STG_Prac_So/Assets/BG_SIDE2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_SIDE2 : MonoBehaviour
+{
+    public float speed;
+    public Vector3 limit;
+    public Vector3 spawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > limit.x)
+        {
+            transform.position = new Vector3(spawn.x, spawn.y, spawn.z);
+        }
+
+        transform.Translate((-1)*speed * Time.deltaTime, 0, 0);
+    }
+}
